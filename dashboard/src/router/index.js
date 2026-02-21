23c9fef6bc824455bee9af9c26f278037377db4e
anak10thn <anak10thn@gmail.com>
2026-02-21 13:59:32 +0700

    chore: switch Vue Router history mode from web history to hash history.

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { isGatewayConfigured } from '../lib/gateway.js'
 import Home from '../views/Home.vue'
 import Chat from '../views/Chat.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/setup',
